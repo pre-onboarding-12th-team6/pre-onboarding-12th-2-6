f0b9054c75f38e3a267624bf61e0ca299a2c5e31
thumbthing <ilscc0902@gmail.com>
2023-09-01 02:30:51 +0900

    Feat: Error 페이지에서 Main 페이지로 리다이렉트 이벤트 추가

--- src/error/ErrorPage.jsx
+++ src/error/ErrorPage.jsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const ErrorPage = () => {
-	return <div>ErrorPage</div>;
+	const navigate = useNavigate();
+
+	const returnToMainPage = useCallback(
+		(e) => {
+			navigate('/');
+		},
+		[navigate],
+	);
+
+	useEffect(() => {
+		window.addEventListener('click', returnToMainPage);
+		return () => {
+			window.removeEventListener('click', returnToMainPage);
+		};
+	});
+
+	return (
+		<div>
+			<div>this is error page</div>
+			<div>React의 이슈 페이지로 돌아가실려면 화면을 클릭 해주세요</div>
+		</div>
+	);
 };
 
 export default ErrorPage;
