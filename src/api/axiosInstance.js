6f5e50218709146abb8a3038fae8aa3186cda9da
slowteady <vbn0213@gmail.com>
2023-09-01 00:11:20 +0900

    style: 파일명, 함수명 직관적으로 변경

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -3,12 +3,12 @@ import axios from 'axios';
 const ACCESS_TOKEN = process.env.REACT_APP_ACCESS_TOKEN;
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 
-const issuesClient = axios.create({
+const axiosInstance = axios.create({
 	baseURL: BASE_URL,
 	timeout: 5000,
 });
 
-issuesClient.interceptors.request.use((config) => {
+axiosInstance.interceptors.request.use((config) => {
 	if (ACCESS_TOKEN) {
 		config.headers.Authorization = ACCESS_TOKEN;
 	}
@@ -16,4 +16,4 @@ issuesClient.interceptors.request.use((config) => {
 	return config;
 });
 
-export default issuesClient;
+export default axiosInstance;
