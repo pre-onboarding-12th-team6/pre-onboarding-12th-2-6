c93442cf9ace9b208fab2febd0c0a5ca08eacd49
iziz9 <46iz.us2@gmail.com>
2023-08-31 20:11:23 +0900

    Design: 상세페이지 디자인 임시 적용

---
+++ src/detail/DetailPage.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
+
+const IssueDetail = () => {
+	const { id } = useParams();
+	const [issueDetail, setIssueDetail] = useState(null);
+	const [loading, setLoading] = useState(false);
+
+	const getIssueDetail = async () => {
+		setLoading(true);
+		// const { data } = await 
+		// setIssueDetail(data);
+		setLoading(false);
+	};
+
+	useEffect(() => {
+		getIssueDetail();
+	}, []);
+
+  return (
+		<Wrapper>
+			<div>
+				<CenteredContainer>
+					<Img src={issueDetail.user.avatar_url} alt="아바타 이미지" />
+					{/* <IssueItem issue={issueDetail} /> */}
+				</CenteredContainer>
+			</div>
+			{/* <Content dangerouslySetInnerHTML={{ __html: htmlBody }} /> */}
+		</Wrapper>
+	);
+};
+
+const Wrapper = styled.div`
+	border: 1px solid var(--color-placeholder);
+	border-radius: 20px;
+	padding: 24px;
+`;
+
+const CenteredContainer = styled.div`
+	display: flex;
+	justify-content: start;
+	align-items: center;
+
+	margin-bottom: 20px;
+`;
+
+const Img = styled.img`
+	height: 20vh;
+	padding-right: 16px;
+`;
+
+const Content = styled.p`
+	border-top: 1px solid var(--color-placeholder);
+	padding-top: 20px;
+`;
+
+export default IssueDetail;
