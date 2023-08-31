457359d0cb70d1141e6efadebc36efcdc2f9f2bd
thumbthing <ilscc0902@gmail.com>
2023-09-01 03:19:57 +0900

    Feat: 에러 페이지 요소 및 navigate 기능 (이전, 메인) 추가

--- src/error/ErrorPage.jsx
+++ src/error/ErrorPage.jsx
@@ -1,27 +1,31 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const ErrorPage = () => {
 	const navigate = useNavigate();
 
-	const returnToMainPage = useCallback(
-		(e) => {
-			navigate('/');
-		},
-		[navigate],
-	);
+	const returnToMainPage = useCallback(() => {
+		navigate('/');
+	}, [navigate]);
 
-	useEffect(() => {
-		window.addEventListener('click', returnToMainPage);
-		return () => {
-			window.removeEventListener('click', returnToMainPage);
-		};
-	});
+	const returnToPreviousPage = useCallback(() => {
+		navigate(-1);
+	}, [navigate]);
 
 	return (
 		<div>
-			<div>this is error page</div>
-			<div>React의 이슈 페이지로 돌아가실려면 화면을 클릭 해주세요</div>
+			<div>
+				<h1>사용할 수 없는 페이지 입니다.</h1>
+			</div>
+			<div>
+				<h3>
+					이용에 불편을 드려 죄송합니다 <br /> 주소를 다시 한 번 확인해주세요
+				</h3>
+			</div>
+			<div>
+				<button onClick={returnToPreviousPage}>이전 페이지로</button>
+				<button onClick={returnToMainPage}>메인 페이지로</button>
+			</div>
 		</div>
 	);
 };
